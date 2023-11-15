62160c83b4645539650ee4495d5b7b7ea09f4850
DobreMihai-echo <dobremihai97@yahoo.com>
2023-11-16 00:03:42 +0100

    modified for Hheorku deploy

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/longcovidspaweb-frontend'));
+app.use(express.static(__dirname + './dist/long-covid-web'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'./dist/longcovidspaweb-frontend/index.html'));
+res.sendFile(path.join(__dirname+'./dist/long-covid-web/index.html'));
 });
