c91a0124533ed651f3424dd908469aa34c94aecf
Alina <gradinarualina0@gmail.com>
2023-12-09 23:02:05 +0200

    Added error-message-popup.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,43 @@
-import { NgModule } from '@angular/core'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatListModule } from '@angular/material/list'
-import { BrowserModule } from '@angular/platform-browser'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { AboutPageComponent } from './about-page/about-page.component'
-import { AppRoutingModule, routingComponents } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { GeneralLayoutComponent } from './general-layout/general-layout.component'
-import { NavbarComponent } from './navbar/navbar.component'
+import { MatListModule } from '@angular/material/list';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GeneralLayoutComponent } from './general-layout/general-layout.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TechnologyPageComponent } from './technology-page/technology-page.component';
-import { FooterPageComponent } from './footer-page/footer-page.component'
+import { FooterPageComponent } from './footer-page/footer-page.component';
+import { ErrorMessagePopupComponent } from './error-message-popup/error-message-popup.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		NavbarComponent,
-		routingComponents,
-		GeneralLayoutComponent,
-		AboutPageComponent,
-		PageNotFoundComponent,
-  TechnologyPageComponent,
-  FooterPageComponent,
-	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		MatListModule,
-		NgbModule,
-		FontAwesomeModule,
-	],
-	providers: [],
-	bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    routingComponents,
+    GeneralLayoutComponent,
+    AboutPageComponent,
+    PageNotFoundComponent,
+    TechnologyPageComponent,
+    FooterPageComponent,
+    ErrorMessagePopupComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    NgbModule,
+    FontAwesomeModule,
+    MatDialogModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
