ee3ee5c8ae1c8fe86b35eda932abb3895c8dedff
DobreMihai-echo <dobremihai97@yahoo.com>
2024-01-10 14:36:00 +0100

    minor changes

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -14,33 +14,47 @@ import { AppConstants } from '../models/appconstants/appconstants.module';
 })
 export class RegisterComponent {
 
-  public signUpForm !: FormGroup;
+  public form !: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private auth:AuthService, private router: Router,private matSnackbar: MatSnackBar){}
 
   ngOnInit(): void {
-    this.signUpForm = this.formBuilder.group({
-      firstName: ["", Validators.required],
-      lastName: ["", Validators.required],
-      username: ["", Validators.required],
-      phoneNumber: ["", Validators.required],
-      email: ["", [Validators.required, Validators.email]],
-      password: ["", [Validators.required, Validators.minLength(8)]]
-    })
+    this.form = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [ Validators.required,
+        Validators.pattern("^[0-9]*$"),
+        Validators.minLength(10), Validators.maxLength(10),
+      ]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator, updateOn: 'submit' });
+  }
+
+  passwordMatchValidator(control: AbstractControl): void {
+    const password = control.get('password')?.value ?? '';
+    const confirmPassword = control.get('confirmPassword')?.value ?? '';
+  
+    if (password !== confirmPassword) {
+      control.get('confirmPassword')?.setErrors({ mismatch: true });
+    }
   }
 
   signUp(){
-    this.auth.register(this.signUpForm).subscribe(
+    this.auth.register(this.form.value).subscribe(
       (response:any) => {
         this.matSnackbar.openFromComponent(SnackbarComponent, {
           data: AppConstants.signinSuccessDetail,
           panelClass: ['bg-danger'],
           duration: 5000
         });
-        this.router.navigate(['login']);
+        this.router.navigate(['/login']);
+
       },
       (error) => {
-        console.log(error);
+        console.log(error)
       }
     )
   }
