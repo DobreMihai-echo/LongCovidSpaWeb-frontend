ac17d7973cb57263d4e767e7ba85d44b30bb41de
DobreMihai-echo <dobremihai97@yahoo.com>
2024-01-10 15:24:38 +0100

    minor changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { SnackbarComponent } from '../components/snackbar/snackbar.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AppConstants } from '../models/appconstants/appconstants.module';
 
 @Component({
 	selector: 'app-login',
@@ -11,33 +15,39 @@ import { Router } from '@angular/router';
 export class LoginComponent {
 	public loginForm!: FormGroup
 
-  constructor(private formbuilder: FormBuilder,private http: HttpClient, private router: Router) { }
+  constructor(private formbuilder: FormBuilder,private http: HttpClient, private router: Router, private auth: AuthService, private matSnackbar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.loginForm = this.formbuilder.group({
-      email: [''],
+      email: ['', Validators.required],
       password: ['', Validators.required]
     })
   }
 
 
   login(){
-	console.log("inside the login")
-    this.http.get<any>("http://localhost:3000/signupUsersList")
-    .subscribe(res=>{
-      const user = res.find((a:any)=>{
-        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password 
-      });
-      if(user){
-        alert('Login Succesful');
-        this.loginForm.reset()
-      this.router.navigate(["home"])
-      }else{
-        alert("user not found")
-      }
-    },err=>{
-      alert("Something went wrong")
-    })
-  }
+    this.auth.login(this.loginForm.value).subscribe(
+      (response:any) => {
+        this.auth.setToken(response.jwtToken);
+        this.auth.setRoles(response.users.authorities)
+        this.auth.setUser(response.users);
+        this.auth.setProfile(response.profilePhoto);
+        this.router.navigate(['/home']);
+        this.matSnackbar.openFromComponent(SnackbarComponent, {
+          data: AppConstants.signinSuccessDetail,
+          panelClass: ['bg-danger'],
+          duration: 5000
+        });
 
+      },
+      (error) => {
+        this.matSnackbar.openFromComponent(SnackbarComponent, {
+          data: error ? error : AppConstants.snackbarErrorContent,
+          panelClass: ['bg-danger'],
+          duration: 5000
+        });
+        console.log(error)
+      }
+    )
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/appconstants/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,74 @@ export class AuthService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public loogin(loginData: any) {
+  public login(loginData: any) {
     return this.httpClient.post(`${this.baseUrl}/api/auth/login`,loginData);
   }
 
   public register(registerData: any) {
     return this.httpClient.post(`${this.baseUrl}/api/auth/register`,registerData);
+  }
+
+  public setRoles(roles:[]) {
+    localStorage.setItem("roles",JSON.stringify(roles));
+  }
+
+  public setUser(user:User) {
+    localStorage.setItem("user",JSON.stringify(user));
+  }
+
+  public getRoles(): [] {
+    const userRoles = localStorage.getItem("roles");
+    return userRoles !== null ? JSON.parse(userRoles) : null;
+  }
+
+  public setToken(token:string) {
+    localStorage.setItem("jwtToken",token);
+  }
+
+  public setProfile(profile:string) {
+    localStorage.setItem("profile",profile);
+  }
+
+  public getToken() {
+    return localStorage.getItem("jwtToken");
+  }
+
+  public clear() {
+    localStorage.clear();
+  }
+
+  public isLoggedIn() {
+    return this.getRoles() && this.getToken();
+  }
+
+  public getUsername() {
+    const user = localStorage.getItem('user');
+    if(user!==null) {
+      return JSON.parse(user).username;
+    }
+    return null;
+  }
+
+  public logout() {
+    localStorage.clear();
+  }
+
+  public roleMatch(allowedRoles: any):boolean {
+    let isMatch = false;
+    const userRoles:any = this.getRoles();
+  
+    if(userRoles != null && userRoles) {
+      for(let i=0;i<userRoles.length; i++) {
+        for(let j=0;j<allowedRoles.length;j++) {
+          if(userRoles[i].authority === allowedRoles[j]) {
+            isMatch = true;
+            return isMatch;
+          } 
+        }
+      }
+    }
+
+    return false;
   }
 }
