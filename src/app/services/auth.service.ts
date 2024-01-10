4e4328cb5c6a46509efbd79d86416251883b121b
DobreMihai-echo <dobremihai97@yahoo.com>
2024-01-10 14:55:48 +0100

    minor changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,13 +6,16 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
+
+  private baseUrl = "http://34.125.41.225";
+
   constructor(private httpClient: HttpClient) { }
 
   public loogin(loginData: any) {
-    return this.httpClient.post(`/api/auth/login`,loginData);
+    return this.httpClient.post(`${this.baseUrl}/api/auth/login`,loginData);
   }
 
   public register(registerData: any) {
-    return this.httpClient.post(`/api/auth/register`,registerData);
+    return this.httpClient.post(`${this.baseUrl}/api/auth/register`,registerData);
   }
 }
